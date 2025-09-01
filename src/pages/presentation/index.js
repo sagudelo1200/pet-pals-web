2736ac1838e4ac9f9c0e0d5d775b2b859abd3aba
Santiago Agudelo <sagudelo1200@gmail.com>
2025-08-31 20:07:40 -0500

    Base Limpia - Pet Pals

---
+++ src/pages/presentation/index.js
@@ -0,0 +1,121 @@
+/*
+=========================================================
+* Material Kit 2 PRO React - v2.1.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-pro-react
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+
+// Material Kit 2 PRO React components
+import MKBox from 'components/MKBox';
+import MKBadge from 'components/MKBadge';
+import MKTypography from 'components/MKTypography';
+import MKButton from 'components/MKButton';
+
+// Material Kit 2 PRO React examples
+import DefaultNavbar from 'examples/Navbars/DefaultNavbar';
+import DefaultFooter from 'examples/Footers/DefaultFooter';
+
+// Presentation page sections
+import Counters from 'pages/presentation/sections/Counters';
+
+// Routes
+import routes from 'routes';
+import footerRoutes from 'footer.routes';
+
+// Images
+import bgImage from 'assets/images/bg-presentation.jpg';
+
+function Presentation() {
+  return (
+    <>
+      <DefaultNavbar
+        routes={routes}
+        /* action={{
+          type: 'external',
+          route: 'https://www.creative-tim.com/product/material-kit-pro-react',
+          label: 'Solicitar paseo',
+          color: 'warning',
+        }} */
+        sticky
+      />
+      <MKBox
+        minHeight="75vh"
+        width="100%"
+        sx={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'top',
+          display: 'grid',
+          placeItems: 'center',
+        }}
+      >
+        <Container>
+          <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
+            <MKTypography
+              variant="h1"
+              color="white"
+              mt={-6}
+              mb={1}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down('md')]: {
+                  fontSize: size['3xl'],
+                },
+              })}
+              textAlign="center"
+            >
+              Muy Pronto en Medellín{' '}
+              <MKBadge
+                badgeContent="Pet Pals"
+                size="lg"
+                variant="contained"
+                color="white"
+                container
+                sx={{ mt: -4 }}
+              />
+            </MKTypography>
+            <MKTypography variant="body1" color="white" textAlign="center" px={6} mt={1}>
+              La tranquilidad de saber que tu mascota siempre estará en buenas manos.
+            </MKTypography>
+
+            <MKBox mt={6}>
+              <MKButton component="a" href="/" color="white" variant="outlined">
+                Quiero ser de los primeros
+              </MKButton>
+            </MKBox>
+          </Grid>
+        </Container>
+      </MKBox>
+      <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -8,
+          mb: 4,
+          backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
+          backdropFilter: 'saturate(200%) blur(30px)',
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      >
+        <Counters />
+      </Card>
+      <MKBox pt={6} px={1} mt={6}>
+        <DefaultFooter content={footerRoutes} />
+      </MKBox>
+    </>
+  );
+}
+
+export default Presentation;
---
+++ src/pages/presentation/sections/Counters.js
@@ -0,0 +1,70 @@
+/*
+=========================================================
+* Material Kit 2 PRO React - v2.1.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-pro-react
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+
+// Material Kit 2 PRO React components
+import MKBox from 'components/MKBox';
+import MKTypography from 'components/MKTypography';
+
+// Material Kit 2 PRO React examples
+import DefaultCounterCard from 'examples/Cards/CounterCards/DefaultCounterCard';
+
+function Counters() {
+  return (
+    <MKBox component="section" py={3}>
+      <MKTypography variant="h3" textAlign="center" px={6} my={1}>
+        Lo que queremos lograr contigo
+      </MKTypography>
+      <Container>
+        <Grid container item xs={12} lg={9} sx={{ mx: 'auto' }}>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={90}
+              suffix="+"
+              color="success"
+              title="👟 Paseadores Verificados 🐕"
+              description="Crear una red de 90+ paseadores certificados en Medellín."
+            />
+          </Grid>
+          <Grid item xs={12} md={4} display="flex">
+            <Divider orientation="vertical" sx={{ display: { xs: 'none', md: 'block' }, mx: 0 }} />
+            <DefaultCounterCard
+              count={600}
+              suffix="+"
+              color="warning"
+              title="🐾 Mascotas Felices ❤️"
+              description="Conectar a más de 600 familias con cuidadores de confianza."
+            />
+            <Divider orientation="vertical" sx={{ display: { xs: 'none', md: 'block' }, ml: 0 }} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={24}
+              suffix="/7"
+              title="📞 Soporte Confiable 🔒"
+              description="Brindar acompañamiento y respuesta en cualquier momento del día."
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Counters;
